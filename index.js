5ab217eddbf450e13606ed23f208777452cde93c
Guillermo Bruce <guillermo@matterport.com>
2019-12-19 16:14:46 -0800

    tweaks
    - disable logs
    - add color to hello bundle

--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 var showcase = document.getElementById('showcase');
 var key = 'YOUR KEY HERE';
 
-showcase.addEventListener('load', function() {
-  showcase.contentWindow.MP_SDK.connect(showcase, key, '3.2')
-    .then(onConnect, onError);
+showcase.addEventListener('load', async function() {
+  let sdk;
+  try {
+    sdk = await showcase.contentWindow.MP_SDK.connect(showcase, '{YOUR SDK KEY}', '3.2');
+  }
+  catch(e) {
+    console.error(e);
+    return;
+  }
+
+  console.log('%c  Hello Bundle SDK! ', 'background: #333333; color: #00dd00');
 });
-
-async function onConnect(sdk) {
-  console.log('Hello Bundle SDK', sdk);
-  // ready to access the sdk!
-}
-
-function onError(error) {
-  console.error(error);
-}
